f53e04441ec0d9970eeea351341cafe91b82a508
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2022-03-31 00:02:47 +0200

    Initial code

--- GetSongsFunction/GetSongsFunction.cs
+++ GetSongsFunction/GetSongsFunction.cs
@@ -1,35 +1,36 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 
 namespace GetSongsFunction
 {
+    public class Song : TableEntity
+    {
+        public string SongName { get; set; }
+        public string SongAlbum { get; set; }
+    }
+
     public static class GetSongsFunction
     {
         [FunctionName("GetSongsFunction")]
+        [StorageAccount("AzureWebJobsStorage")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [Table("SongsTable", Connection = "AzureWebJobsStorage")] CloudTable table,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Getting information from Songs Table!");
 
-            string name = req.Query["name"];
+            var tableQuery = new TableQuery<Song>();
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var tableContents = await table.ExecuteQuerySegmentedAsync(tableQuery, null);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(tableContents.Results);
         }
     }
 }
